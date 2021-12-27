ec576b1c2fa55aa85e026638d2f7df759f41fc62
Chibby124 <Dwysocki@manor.edu>
2021-12-26 19:25:50 -0500

    Working version v2

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -29,9 +29,7 @@ export class UserService {
   }
   constructor(private http: HttpClient) { }
 
-  getPublicContent(): Observable<any> {
-    return this.http.get(API_URL + 'all', { responseType: 'text' });
-  }
+
 
   getUserBoard(): Observable<any> {
     return this.http.get(API_URL + 'users', { responseType: 'text' });
